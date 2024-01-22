251b00d785f200983639d9a08393b9659274970b
LAPTOP-CQOG6PAO\Playdata <null00007@naver.com>
2024-01-22 12:24:50 +0900

    API time 패키지 실습
    java.util.Date, java.sql.Date, java.util.Calendar 패키지 실습

---
+++ chap09-api-lecture-source/src/main/java/com/ohgiraffers/section06/time/Application05.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.section06.time;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Applicaion05 {
+    public static void main(String[] args) {
+
+        /* 수업목표. time 패키지의 클래스들에 포맷팅을 적용하여 출력할 수 있다. */
+        /* 설명. 문자열을 time 패키지 자료형으로 파싱할 수 있다.(사용 인식 가능한 문자열 패턴) */
+        /* 필기. 날짜를 다룰 때에는 문자열로 저장하여 파싱을 사용하는 것이 용이함. */
+        String timeNow = "14:05:10";
+        String dateNow = "2022-10-12";
+
+        LocalTime localTime = LocalTime.parse(timeNow);
+        LocalTime localDate = LocalTime.parse(dateNow);
+        LocalDateTime localDateTime = LocalDateTime.parse(dateNow + "T" + timeNow);
+
+        System.out.println(localTime);
+        System.out.println(localDate);
+        System.out.println(localDateTime);
+
+        /* 설명. 기본 패턴이 아닌 경우 */
+        String timeNow2 = "14.05.10";
+        String dateNow2 = "221003";
+
+        LocalTime localTime2 = LocalTime.parse(timeNow2, DateTimeFormatter.ofPattern("HH-mm-ss"));
+        LocalTime localDate2 = LocalTime.parse(dateNow2, DateTimeFormatter.ofPattern("yyMMdd"));
+
+        System.out.println(localTime2);
+        System.out.println(localDate2);
+
+        String dateFormat = localDate2.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String timeFormat = localTime2.format(DateTimeFormatter.ofPattern("HH:mm"));
+
+        System.out.println(dateFormat);
+        System.out.println(timeFormat);
+
+    }
+}
